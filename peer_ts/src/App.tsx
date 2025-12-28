37bb23e809c7826ef552a14c85d2d55f372d34b3
RoadRoot01 <gunks20011@naver.com>
2025-12-28 19:56:59 +0900

    도훈+도근 짬뽕

--- peer_ts/src/App.tsx
+++ peer_ts/src/App.tsx
@@ -82,7 +82,7 @@ const config = {};
 function App() {
     console.log('Rendering... ');
     let changeCount = 0;
-    // const room = 'testRoom'; // Example room name
+    const room = 'testRoom'; // Example room name
 
     // Record<<K,T> : TS utility type
 
@@ -278,11 +278,13 @@ function App() {
             console.log('[Peer] Connected to signaling server');
             if (MODE === '1_TO_N') {
                 console.log('[Peer] Joining room in 1_TO_N mode:', room);
-                socketRef.current?.emit('join-1_to_n', room);
+                //socketRef.current?.emit('join-1_to_n', room);
+                socketRef.current?.emit('join', { room, type: '1_to_n' });
             }
             else if (MODE === 'MESH') {
                 console.log('[Peer] Joining room in MESH mode:', room);
-                socketRef.current?.emit('join-mesh', room);
+                //socketRef.current?.emit('join-mesh', room);
+                socketRef.current?.emit('join', { room, type: 'mesh' });
             }
         });
 
@@ -562,10 +564,11 @@ function App() {
 
         // 연결 상태 모니터링
         pc.onconnectionstatechange = async () => {
-            console.log(`[${peerId}] state:`, pc.connectionState);
+            //console.log(`[${peerId}] state:`, pc.connectionState);
+            console.log(`[${peerId}]${pc.connectionState} state:`, pc.connectionState);
             if (pc.connectionState === 'disconnected' || pc.connectionState === 'closed') {
-                console.log(`[${peerId}] Connection ${pc.connectionState}. ${pcTypesRef.current[peerId]}Attempting renegotiate.`);
-                // 재연결 시도 join-redial for recvonly connections
+                console.log(`[${peerId}] Connection . ${pcTypesRef.current[peerId]}Attempting renegotiate.`);
+                // 재연결 시도 join-redial for offerer connections
                 const pcType = pcTypesRef.current[peerId];
                 // Offerer 인지 확인 후 renegotiate 
                 if (pcType === 'offerer') {
@@ -573,24 +576,46 @@ function App() {
                 }
             }
             else if (pc.connectionState === 'failed') {
-                // 재연결 시도(pc.restartIce();? 하드리셋 or 소프트 리셋)
+                /*
+                // 재연결 시도
                 console.log(`[${peerId}] Connection failed. Attempting to restart ICE...`);
                 try {
-                    // console.log(`[${peerId}] Soft Resetting...`);
-                    // console.log(`[${peerId}] Hard Resetting...`);
-                    // await softReset(peerId);
-                    // candidate flush
-
                     pendingCandRef.current[peerId] = []
-
-                    // await hardReset(peerId);
-
-                    /* 1029 추가 : 연결 실패 했을 때 담아두었던 candidate배열 다시 전송*/
+                    // 1029 추가 : 연결 실패 했을 때 담아두었던 candidate배열 다시 전송
                     sendIceCandidate(peerId);
                 }
                 catch (e) {
-                    // hardReset
                     console.error(e);
+                }
+                */
+
+                // 재연결 시도 Redial
+                // console.log(`[${peerId}] Connection failed. Attempting to restart ICE...`);
+                const pcType = pcTypesRef.current[peerId];
+                if (pcType === 'offerer') {
+                    try {
+                        const targetPc = pcsRef.current[peerId];
+                        console.log(`[${peerId}] offerer connection lost. Attempting redial.`);
+                        if (targetPc) {
+                            targetPc.close();
+                            delete pcsRef.current[peerId];
+                            delete pcTypesRef.current[peerId];
+                            pendingCandRef.current[peerId] = [];
+                            iceCandidateGatheredArrayRef.current[peerId] = [];
+                            setUsers(prev => prev.filter(u => u.id !== peerId));
+                        }
+                        else {
+                            console.log(`[${peerId}] No existing peer connection found for hard reset.`);
+                        }
+                        console.log(`[${peerId}] Redialing...`);
+                        socketRef.current?.emit('join', { room: room, type: 'redial', to: peerId });
+                    }
+                    catch (e) {
+                        console.error(e);
+                    }
+                }
+                else {
+                    console.log(`[${peerId}] Non-offerer connection failed. Closing peer connection.`);
                 }
             }
             /*
---
+++ server_new.js
@@ -0,0 +1,157 @@
+const fs = require('fs');
+const https = require('https');
+const express = require('express');
+// const WebSocket = require('ws');
+const path = require('path');
+
+const app = express();
+
+// HTTPS 인증서
+const options = {
+    key: fs.readFileSync('C:\\Windows\\System32\\key.pem'), // <kau> added to resolve https issues
+    cert: fs.readFileSync('C:\\Windows\\System32\\cert.pem')
+};
+
+// HTTPS 서버 생성
+const httpsServer = https.createServer(options, app);
+
+// 정적 파일 (index.html 등)
+app.use(express.static(__dirname));
+
+// WebSocket 서버 붙이기
+
+httpsServer.listen(8000, '0.0.0.0', () => {
+    console.log(' HTTPS server running');
+});
+
+/* 외부 접속시 https://192.168.0.3:8000 when opening html */
+// const WebSocket = require('ws');
+// const server = new WebSocket.Server({ port: 8000 , host: '0.0.0.0'});
+
+const { Server } = require('socket.io');
+const io = new Server(httpsServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+const rooms = new Map();
+const peers = new Map();
+
+io.on('connection', socket => {
+    const id = Math.random().toString(36).substr(2, 9); // generate random ID
+    peers.set(id, socket);
+    console.log(`[Server] New connection: ${id}`);
+
+    socket.on('join-mesh', room => {
+        if (!rooms.has(room)) {
+            console.log(`[Server] Room ${room} does not exist, creating new room.`);
+            rooms.set(room, new Set());
+        }
+        rooms.get(room).add(id);                 // 핵심
+
+        socket.join(room);
+
+        // 본인 id 전송 0906
+        socket.emit('my-id', id);
+
+        // Mesh를 위한 로직. 새로운 참가자가 오면, 새로운 참가자는 기존 참가자 목록(existing-peers)을 받아 각각과 연결을 시도하고,
+        // 기존 참가자들에게는 new-peer 이벤트를 보내 그들 쪽에서도 연결을 준비하게 함.
+
+        // 기존 참가자 목록 전송  (나 자신 제외)
+        const existingPeers = [...peers.keys()].filter(peerId => peerId !== id);
+        console.log(`[Server] Existing peers in room ${room}:`, existingPeers);
+        socket.emit('existing-peers', existingPeers);
+
+        // 모두에게 new peer 이벤트 전송 (나 자신 제외)
+        // console.log(`[Server] Peer ${id} joined room ${room}`);
+
+        // 디버깅 로그
+        console.log(`[room:${room}] join ->`, id);
+
+        // room에 참가한 소켓에 room 정보 저장, data는 기본적으로 {}, 원하는 key 추가가능
+        if (!socket.data) socket.data = {};
+        socket.data.room = room;
+
+    });
+
+    socket.on('join-1_to_n', room => {
+        if (!rooms.has(room)) {
+            console.log(`[Server] Room ${room} does not exist, creating new room.`);
+            rooms.set(room, new Set());
+        }
+        rooms.get(room).add(id);                 // 핵심
+
+        socket.join(room);
+
+        // 본인 id 전송 0906
+        socket.emit('my-id', id);
+
+        const existingPeers = [];
+        // 맨처음 참가자만 전달  (나 자신 제외)
+        if (peers.size > 1) {
+            const allKeys = [...peers.keys()]; // 모든 키를 배열로 변환
+            const firstKey = allKeys[0];      // 배열의 첫 번째 요소 접근
+            console.log(`[Server] First peer in room ${room} `, firstKey);
+            existingPeers.push(firstKey);
+        }
+        else if (peers.size <= 1) {
+            console.log(`[Server] No existing peers in room ${room}`);
+        }
+
+        console.log(`[Server] Existing peers in room ${room}:`, existingPeers);
+        socket.emit('existing-peers', existingPeers);
+
+        // 모두에게 new peer 이벤트 전송 (나 자신 제외)
+        socket.to(room).emit('new-peer', id);
+        // console.log(`[Server] Peer ${id} joined room ${room}`);
+
+        // 디버깅 로그
+        console.log(`[room:${room}] join ->`, id);
+        // room에 참가한 소켓에 room 정보 저장, data는 기본적으로 {}, 원하는 key 추가가능
+        if (!socket.data) socket.data = {};
+        socket.data.room = room;
+
+    });
+
+    // offer 전달
+    socket.on('offer', ({ to, data }) => {
+        const targetSocket = peers.get(to);
+        targetSocket.emit('offer', { from: id, data });
+        console.log(`[Server] Offer from ${id} to ${to}`);
+    });
+
+    // answer 전달
+    socket.on('answer', ({ to, data }) => {
+        // peers 객체에서 to에 해당하는 소켓을 찾음
+        const targetSocket = peers.get(to);
+        targetSocket.emit('answer', { from: id, data });
+        console.log(`[Server] Answer from ${id} to ${to}`);
+    });
+
+    //<kau> After a user received signal info by offer and answer, it sends the its candidate info to the another peer
+    socket.on('candidate', ({ to, data }) => {
+        const targetSocket = peers.get(to);
+        targetSocket.emit('candidate', { from: id, data });
+        console.log(`[Server] Candidate from ${id} to ${to}`);
+    })
+
+    // 추가 구현 했다고 되어있음. 네트워크 연결이 불안정하거나 늦게 붙는 경우를 대비해서 모아둔 candidate들을 한 번에 배열로 보내는 로직을 추가 구현한건가?
+    socket.on('candidateArray', ({ to, data }) => {
+        const targetSocket = peers.get(to);
+        targetSocket.emit('candidateArray', { from: id, data });
+        console.log(`[Server] Candidate from ${id} to ${to}`);
+    })
+
+    socket.on('disconnect', () => {
+        const room = socket.data.room;
+        if (room && rooms.has(room)) {
+            const peer = rooms.get(room);
+            peer.delete(id);
+            socket.to(room).emit('disconnected', id);
+            console.log(`[Server] Peer ${id} disconnected from room ${room}`);
+        }
+        peers.delete(id);                        // 핵심
+    });
+});
